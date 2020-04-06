a0cd9174a855eea24faf4cc852006c82d46c3868
AlexandreCourtin <alexandre.c.ext@padoa-group.com>
2020-04-06 18:06:44 +0200

    first experiment

---
+++ Assets/Scripts/BoidGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidGenerator : MonoBehaviour
+{
+    public GameObject boidObj;
+
+    void Start() {
+        float rot = 7.5f;
+
+        for (int i = 0 ; i < 50 ; i++) {
+            for (int j = 0 ; j < 50 ; j++) {
+                GameObject c = Instantiate(boidObj, Vector3.zero, Quaternion.identity);
+                c.transform.eulerAngles = new Vector3(0f, -j * rot, -i * rot);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BoidScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoidScript : MonoBehaviour
+{
+    public float speed = 1f;
+    public float rotSpeed = 1f;
+
+    float boundLimit = 29f;
+
+    void FixedUpdate() {
+        transform.position = transform.position + (transform.up * speed * Time.fixedDeltaTime);
+        transform.Rotate(Random.Range(-rotSpeed, rotSpeed) * Time.fixedDeltaTime,
+            Random.Range(-rotSpeed, rotSpeed) * Time.fixedDeltaTime,
+            Random.Range(-rotSpeed, rotSpeed) * Time.fixedDeltaTime,
+            Space.Self);
+
+        checkBounds();
+    }
+
+    void checkBounds() {
+        // X
+        if (transform.position.x > boundLimit) {
+            transform.position = new Vector3(transform.position.x - boundLimit * 2,
+                transform.position.y,
+                transform.position.z);
+        } else if (transform.position.x < -boundLimit) {
+            transform.position = new Vector3(transform.position.x + boundLimit * 2,
+                transform.position.y,
+                transform.position.z);
+        }
+        // Y
+        if (transform.position.y > boundLimit) {
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y - boundLimit * 2,
+                transform.position.z);
+        } else if (transform.position.y < -boundLimit) {
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y + boundLimit * 2,
+                transform.position.z);
+        }
+        // Z
+        if (transform.position.z > boundLimit) {
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y,
+                transform.position.z - boundLimit * 2);
+        } else if (transform.position.z < -boundLimit) {
+            transform.position = new Vector3(transform.position.x,
+                transform.position.y,
+                transform.position.z + boundLimit * 2);
+        }
+    }
+}
